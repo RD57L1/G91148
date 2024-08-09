9f51a9b0a72874ae333180a6d2b9d3e0560e554e
RD57L1 <rodrigo>
2024-08-08 21:19:25 -0300

    AULA2_ATIVIDADE1

---
+++ Aula1_teste1.js
@@ -0,0 +1,9 @@
+//TESTE 1
+
+var nome = "Rodrigo"
+let idade = 23; 
+var tem_pet = 'sim'
+
+console.log("Escreva seu nome:", nome);
+console.log("Informe sua idade: ",idade,"anos");
+console.log("Você tem pet?",tem_pet);
---
+++ Aula1_teste3.js
@@ -0,0 +1,8 @@
+//TESTE 3
+
+const prompt = require('prompt-sync')();
+const nome = prompt("Digite seu nome:");
+const idade = prompt("Digite sua idade:");
+const tem_pet = prompt ("Você tem um pet?");
+
+console.log("Meu nome é" ,nome, ", Tenho",idade," anos," ,tem_pet, "tenho pet!" )
