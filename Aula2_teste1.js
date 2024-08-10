f0b3ce83c1025db47a0fd912a1129759f13e23ab
RD57L1 <rodrigo>
2024-08-09 21:26:50 -0300

    Aula2

---
+++ Aula2_teste1.js
@@ -0,0 +1,32 @@
+//Declare duas variáveis,'a' e 'b'. e atribua a elas valores numéricos.  
+
+var a = 10
+var b = 5
+
+var soma = (a+b) 
+var subtracao = (a-b)
+var multiplicacao = (a*b)
+var divisao = (a/b)
+//Calcule e imprima no console a soma, subtração, multiplicação e divisão.
+
+console.log ('Soma:' + soma)
+console.log ('Subtração:' + subtracao )
+console.log ('Multiplicação:' + multiplicacao)
+console.log ('Divisão:' + divisao)
+
+
+// Declare duas variáveis 'X' e 'Y'. Atribua a 'X' um número e a 'Y' uma string que representa um número.
+
+var x = 15
+var y = b
+
+//compare 'x' e 'y' usando os operadores de comparação e imprima os resultados no console.
+
+console.log ("O valor das respectivas variaveis 'x' e 'y' são", x, 'e',y)
+
+if(x > y){
+    console.log ('A variável x é maior')
+}
+else{
+    console.log('A variável y é maior')
+}
