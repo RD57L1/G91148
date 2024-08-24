8d0785e54e8e544c6f577cf1901e90985b2e6b40
RD57L1 <rodrigo>
2024-08-23 21:12:57 -0300

    aula4

---
+++ aula4exemp1.js
@@ -0,0 +1,35 @@
+/* 
+
+let frutas = ['Maçã','Banana','Manga','Uva'];
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
+
+*/
+
+
+/*
+let aluno = {
+    'nome': 'Rodrigo',
+    'idade':23,
+    'curso': 'Ciência da computação'
+};
+
+for (let chave in aluno) {
+    console.log(`Chave:${chave},Valor:${aluno[chave]}`);
+}
+
+*/
+
+/*
+for(let i = 0; i < 11; i++) {
+    console.log(i); // Vai imprimir os números de 0 a 10
+}
+
+*/
+
+let i = 0;
+while (i < i + 1) {
+    console.log(i); // Vai imprimir os números de 0 a 10
+    i++;
+}
