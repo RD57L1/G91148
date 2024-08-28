cac57d5f0ea551b8ecf274625916418496e96b8b
RD57L1 <rodrigo>
2024-08-27 21:14:51 -0300

    Aula5

---
+++ aula5Exemplo2.js
@@ -0,0 +1,38 @@
+/*
+
+// Exercicio Loop For
+
+const preço = [ 16.55, 50.00, 75.00, 100.00, 25.90]
+const promocao = []
+const naoPromocao = []
+
+
+    for (let i = 0; i < preço.length; i++) {
+        if (preço[i] < 50) {
+            promocao.push(preço[i]);
+        } else {
+            naoPromocao.push(preço[i]);
+        }
+    }
+    
+    console.log("Produtos em promoção:", promocao);
+    console.log("Produtos sem promoção:", naoPromocao);
+    
+
+*/
+
+// Exercicio Loop While
+
+poupanca = 0
+objetivo = 5000
+
+while (poupanca <= objetivo){
+    console.log(poupanca); 
+    poupanca += 500;
+
+} 
+
+if (poupanca = objetivo) {
+    console.log('parabéns!');
+
+}
