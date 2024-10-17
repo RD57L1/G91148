fff98f1b5b3be4ea04e892154ec2e1b07d435f29
RD57L1 <rodrigo>
2024-10-16 21:06:30 -0300

    Aula16/10/24

--- jsExpress/teste.js
+++ jsExpress/teste.js
@@ -3,18 +3,34 @@ const express = require('express');
 const app = express();
 const porta = 8080;
 
+// Middleware para parsear dados JSON
 app.use(express.json());
 
+// Rota POST /login
+app.post('/login', async (req, res) => {
 
-// Base de dados simulada (substituir por um banco de dados real)
-app.post('/dados', (req, res) => {
-  const usuarios = req.body;
-  console.log('Dados recebidos:', usuarios);
-res.send("Dados: OK");
+  const { email, senha } = req.body;
+
+console.log("Dados Recebido.");
+res.send("Dados OK");
+
+  console.log("LOGIN");
+  console.log("Email: "+email);
+  console.log("Senha: "+senha);
+
+
+
+  // Encontrar o usuário na base de dados
+  const usuario = user.suarios.find(u => u.email === email && u.senha === senha);
+
+  if (usuario) {
+    res.json({ message: "Bem-vindo, "+usuario.nome });
+  } else {
+    res.status(401).json({ error: 'Credenciais inválidas' });
+  }
 });
 
 
 app.listen(porta, () => {
   console.log(`Start server: http://localhost: ${porta}`);
 });
-
